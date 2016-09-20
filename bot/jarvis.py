a2129de825d949931e3b2a4dc86cdcfc3baf9f97
Vivek Singh <vivek@happay.in>
2016-09-20 20:07:53 +0530

    fixed some issue

--- bot/jarvis.py
+++ bot/jarvis.py
@@ -1,8 +1,8 @@
 import os
 import time
 from slackclient import SlackClient
-from wit import parse_wit_response, make_requests
-from cofig import BOT_ID
+from wit import parse_wit_response, make_requests, parse_converse_response
+from config import BOT_ID, SLACK_BOT_TOKEN
 
 # starterbot's ID as an environment variable
 # BOT_ID = os.environ.get("BOT_ID")
@@ -13,50 +13,50 @@ EXAMPLE_COMMAND = "do"
 
 
 # instantiate Slack & Twilio clients
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+slack_client = SlackClient(SLACK_BOT_TOKEN)
 
 def handle_command(command, channel):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If so, then acts on the commands. If not,
-        returns back what it needs for clarification.
-    """
-    response = "Not sure what you mean. Did you forget to add @jarvis\
- before your message."
+	"""
+		Receives commands directed at the bot and determines if they
+		are valid commands. If so, then acts on the commands. If not,
+		returns back what it needs for clarification.
+	"""
+	response = "Not sure what you mean. Did you forget to add @jarvis \
+before your message."
 	if command and channel:
-	    wit_response = make_requests(command)
-	    response_text = parse_converse_response(wit_response)
-	    if response_text == '':
-	        response_text = response
+		wit_response = make_requests(command)
+		response_text = parse_converse_response(wit_response)
+		if response_text == '':
+			response_text = response
 	else:
 		response_text = response
 	slack_client.api_call("chat.postMessage", channel=channel,
-	                          text=response_text, as_user=True)
+							  text=response_text, as_user=True)
 
 
 
 def parse_slack_output(slack_rtm_output):
-    """
-        The Slack Real Time Messaging API is an events firehose.
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
-    """
-    output_list = slack_rtm_output
-    if output_list and len(output_list) > 0:
-        for output in output_list:
-            if output and 'text' in output and AT_BOT in output['text']:
-                # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
-                       output['channel']
-    return None, None
+	"""
+		The Slack Real Time Messaging API is an events firehose.
+		this parsing function returns None unless a message is
+		directed at the Bot, based on its ID.
+	"""
+	output_list = slack_rtm_output
+	if output_list and len(output_list) > 0:
+		for output in output_list:
+			if output and 'text' in output and AT_BOT in output['text']:
+				# return text after the @ mention, whitespace removed
+				return output['text'].split(AT_BOT)[1].strip().lower(), \
+					   output['channel']
+	return None, None
 
 if __name__ == "__main__":
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
-    if slack_client.rtm_connect():
-        print("StarterBot connected and running!")
-        while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
-            handle_command(command, channel)
-            time.sleep(READ_WEBSOCKET_DELAY)
-    else:
-        print("Connection failed. Invalid Slack token or bot ID?")
+	READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+	if slack_client.rtm_connect():
+		print("StarterBot connected and running!")
+		while True:
+			command, channel = parse_slack_output(slack_client.rtm_read())
+			handle_command(command, channel)
+			time.sleep(READ_WEBSOCKET_DELAY)
+	else:
+		print("Connection failed. Invalid Slack token or bot ID?")
